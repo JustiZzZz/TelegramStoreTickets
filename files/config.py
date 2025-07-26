82e461fdac5d6d06d9ed5b4d11b1d95a68e212fd
tanyakvitka <137199576+tanyakvitka@users.noreply.github.com>
2025-07-27 01:18:09 +0300

    Add files via upload

--- files/config.py
+++ files/config.py
@@ -1,5 +1,14 @@
-
 TOKEN = ''
+
+ADMIN_ID = ''
+
+SUCCESS_PAYMENT_MESSAGE = (
+    "✅ <b>Оплата подтверждена!</b>\n\n"
+    "Благодарим за доверие. Вот ваша персональная ссылка-приглашение для доступа к каналу. Она сработает только один раз\n\n"
+    "Желаем вам успешной подготовки и высоких результатов на экзамене! ✨\n\n"
+    "⬇️ Ваша ссылка ⬇️\n"
+    "{link}"
+)
 
 MEDIA_MATH = [
     'AgACAgIAAxkBAANzaITm8TdCGS6vG98RfH5jsFU2_SwAAjLzMRsj3SlIWXcP-ZMh8roBAAMCAAN5AAM2BA',
@@ -8,4 +17,11 @@ MEDIA_MATH = [
 
 VOICE_MATH = [
     'AwACAgIAAxkBAANraITlxvsacnVVVmT0JeDGkmhNpbwAAmp1AAKJHSBIrU1nggABZokJNgQ'
+]
+
+MEDIA_PHYSICS = [
+    'AgACAgIAAxkBAAIDLWiFTK4GZOYjdiBhkS7n6xWS5zJDAAIH8TEb1w0xSHuFpXJOpwPmAQADAgADeQADNgQ'
+]
+VOICE_PHYSICS = [
+    'AwACAgIAAxkBAAIDL2iFTPAZ7xLeRXmPFpj3IQ5P3IqyAAJcfwACv30oSOneBVdiHLGGNgQ'
 ]
